b0a09e4a6ee512772662795bf62e674f228dfbad
dhruv <dhruvnaveen.440@gmail.com>
2023-04-20 01:02:03 +0530

    Add: login route and controller

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -24,4 +24,31 @@ router.post("/signup", (req, res) => {
   res.status(200).json({ USERS, status: "ok" });
 });
 
+router.post("/login", function (req, res) {
+  const { email, password } = req.body || {};
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ status: "failure", error: "Please enter all the credentials" });
+  }
+
+  const user = USERS.find((user) => user.email === email);
+  if (!user)
+    return res.status(400).json({
+      status: "failure",
+      error: "User doesn't exist",
+    });
+  else if (user.password !== password)
+    return res.status(401).json({
+      status: "failure",
+      error: "Wrong Password!",
+    });
+
+  res.status(200).json({
+    data: { name: user.name, email: user.email, token: "adwdawd" },
+    status: "ok",
+  });
+});
+
 module.exports = router;
